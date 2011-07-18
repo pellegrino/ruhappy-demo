286a9b48ee9179da721d2b9ec6ac35c505c11daa
Vitor Pellegrino <vitorp@gmail.com>
2011-07-18 20:10:58 -0300

    Generating rails 3.0.9 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuhappyDemo::Application.config.secret_token = '3518207e0e5f3bfdf2871d35c0b62864f50fe4be183a602bcfbbbd1f99e70c504e66c9c2fcdf7ba4a40fcbc324387e092561c81daadca89879ef3b95664e0cdc'
